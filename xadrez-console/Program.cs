c55f3775ce8e7193797b513db220b79293333b49
Alesandro Diogo <alesandromendesdiogo24@gmail.com>
2024-02-23 11:35:12 +0100

    criando a classe posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Tabuleiro;
+
+namespace xadrez_console;
+        class Program
+    {
+        static void Main(string[] args)
+        {
+        Posicao p;
+        p = new Posicao(3, 4);
+
+        Console.WriteLine("Posicao: " +p);
+    }
+    }
+    
